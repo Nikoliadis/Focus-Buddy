1080bc8c9a3ae60b7b202fb1dcc19fb90ed14ea2
Nikoliadis <nnikoliadis@nyc.gr>
2026-02-09 11:14:17 +0200

    Add PostgreSQL setup and update gitignore

--- app.py
+++ app.py
@@ -2,18 +2,26 @@ from datetime import datetime
 from flask import Flask
 from config import Config
 
+from main.db import db
 from main import main_bp
+
+from models.user import User
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
+
     @app.context_processor
     def inject_globals():
         return {"year": datetime.now().year}
 
     app.register_blueprint(main_bp)
+
+    with app.app_context():
+        db.create_all()
 
     return app
 
